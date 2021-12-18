19989c798c74cd081dbf9ecdbab6cf890f789eef
Leandro <leandrovillafu@gmail.com>
2021-12-18 14:42:01 -0300

    lorem ipsum

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Fusce tincidunt dui ac felis
+          ultricies, accumsan porttitor leo molestie. Ut eget odio lorem.
+          Aliquam ut ante egestas, imperdiet est eget, mattis metus. Integer nec
+          lectus nisi. Proin at nulla magna. Donec accumsan orci eget urna
+          mollis tempus. Duis placerat tempus erat eget dapibus.
         </p>
         <a
           className="App-link"
